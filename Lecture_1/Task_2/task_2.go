6fb2bca4765fa64c7e822b6446c24ab3de5dfc3a
alpdk <alexander@podkopaev.net>
2026-07-26 14:35:04 +0200

    Solved task 2 from lecture 1

---
+++ Lecture_1/Task_2/task_2.go
@@ -0,0 +1,35 @@
+/*
+	Task name: Max number
+*/
+
+package main
+
+import "net"
+import "strconv"
+import "math"
+
+func main() {
+	ip := net.IPv4(13, 61, 191, 208)
+	host := net.UDPAddr{IP: ip, Port: 8090}
+	conn, err := net.DialUDP("udp", nil, &host)
+
+	if err != nil {
+		print("AAAA ERROR!!!")
+		return
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 100)
+	conn.Write([]byte("Numbers"))
+
+	max_value := math.MinInt32
+
+	for range 10 {
+		le, _ := conn.Read(buffer)
+		value, _ := strconv.Atoi(string(buffer[:le]))
+
+		max_value = max(max_value, value)
+	}
+
+	println(max_value)
+}
